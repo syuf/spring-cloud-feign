df10aea02246258c75087b105be1fec62dbb73dc
syuf <syuf@DESKTOP-QUH3PA5>
2018-06-24 18:52:35 +0800

    初始化项目

---
+++ spring-cloud-feign/src/test/java/com/zlpay/eurekac/SpringCloudEurekaCApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zlpay.eurekac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudEurekaCApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
