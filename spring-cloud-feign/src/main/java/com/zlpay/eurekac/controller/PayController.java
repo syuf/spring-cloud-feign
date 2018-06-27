557b13e94b3fa6545bdcf2d14173bc87d9aeec8d
syuf <356418670@qq.com>
2018-06-28 01:06:18 +0800

    添加config功能

--- spring-cloud-feign/src/main/java/com/zlpay/eurekac/controller/PayController.java
+++ spring-cloud-feign/src/main/java/com/zlpay/eurekac/controller/PayController.java
@@ -1,6 +1,7 @@
 package com.zlpay.eurekac.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,14 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zlpay.eurekac.app.HiAppService;
 
-@RestController("pay")
+@RestController
 public class PayController {
 
+	@Value("${username}")
+    String content;
+	
 	@Autowired
 	HiAppService schedualServiceHi;
 	
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     public String sayHi(@RequestParam String name){
         return schedualServiceHi.sayHiFromClientOne(name);
+    }
+    
+    @RequestMapping(value = "/read",method = RequestMethod.GET)
+    public String read(){
+        return content;
     }
 }
